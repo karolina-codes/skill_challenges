2612e6fedcdf9b448e8e73f64e8691754123e6ac
Karolina Joksaite <k.joksaite00@gmail.com>
2022-06-15 12:33:24 +0100

    edited check_grammar method to check punctuation and capitalisation + tests

--- lib/check_grammar.rb
+++ lib/check_grammar.rb
@@ -1,9 +1,13 @@
 def check_grammar(text)
-  if (text =~ /[A-Z]/) == 0
-    return "All good, the text is capitalised."
+  if (text =~ /[A-Z]/) == 0 && /[?!.]/.match?(text, -1)
+    puts 'All good, the text is capitalised.' 
+    puts 'Your punctuation is on point.'
   else 
-    fail "The first word is not capitalised."
+    fail 'The first word is not capitalised and/or your punctuation is incorrect.'
   end
+
+  true
 end
+
 
 
--- spec/check_grammar_spec.rb
+++ spec/check_grammar_spec.rb
@@ -1,13 +1,16 @@
 require 'check_grammar'
 
 RSpec.describe 'check_grammar method' do 
-  it 'checks if text is capitalised' do 
-    result = check_grammar("Today is a nce day")
-    expect(result).to eq "All good, the text is capitalised."
+  context 'when text is capitalised and end of text has appropriate puncutuation' do
+    it 'returns true' do 
+     result = check_grammar('Today is a nice day.')
+     expect(result).to eq true
+    end
   end
-  context "text is not capitalised"
-  it "fails" do
-    expect {check_grammar("today is a nice day") }.to raise_error "The first word is not capitalised."
-  end 
+  context 'when text is not capitalised and text does not end with appropriate punctuation 'do
+    it 'fails' do
+      expect { check_grammar('today is a nice day') }.to raise_error 'The first word is not capitalised and/or your punctuation is incorrect.'
+    end 
+  end
 end
   
