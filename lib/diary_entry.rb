1fb3a50a7495615529157cc6793f5fc3095b68b3
Karolina Joksaite <k.joksaite00@gmail.com>
2022-06-20 15:36:36 +0100

    reformmated & refactored

--- lib/diary_entry.rb
+++ lib/diary_entry.rb
@@ -1,9 +1,8 @@
 class DiaryEntry
-  def initialize(title, contents) # title, contents are strings
-    # initializing at the top is preferable as it allows you to see the variables used from the beginning
+  def initialize(title, contents)
     @title = title
     @contents = contents
-    @stored_index = 0
+    @words_to_read = nil
   end
 
   def title
@@ -22,38 +21,23 @@ class DiaryEntry
     count_words / wpm
   end
 
-  def reading_chunk(wpm, minutes) # both inputs are integers
-    words_to_read = wpm * minutes
+  def reading_chunk(wpm, minutes)
+    size_of_chunk = wpm * minutes
     contents_arr = @contents.split
 
-    if @stored_index.zero? || @stored_index >= contents_arr.size
-      # instance variable = nil for first chunk as it's not initialized || restart from beginning if all words have been read
-      @stored_index = words_to_read # storing the index of words to read for future chunks
+    if !@words_to_read || @words_to_read >= contents_arr.size
+      @words_to_read = size_of_chunk
 
-      first_chunk = contents_arr[0...words_to_read].join(" ")
+      first_chunk = contents_arr[0...size_of_chunk].join(" ")
     else
-      last_word_index = @stored_index + words_to_read # combining stored index from previous chunk + new amount of words to read
-      next_chunk = contents_arr[@stored_index...last_word_index].join(" ") # creating the new chunk
-
-      @stored_index = last_word_index #storing the index that we need to start from for the next chunk
+      last_word = @words_to_read + size_of_chunk
+      next_chunk = contents_arr[@words_to_read...last_word].join(" ")
+      @words_to_read = last_word
 
       next_chunk
     end
   end
 end
 
-entry =
-  DiaryEntry.new(
-    "Sunday",
-    "Today is really hot so I went and got some green juice. It was really good and I will probably get some next week as well."
-  )
-
-puts entry.reading_chunk(1, 4)
-puts entry.reading_chunk(1, 4)
-puts entry.reading_chunk(1, 4)
-puts entry.reading_chunk(1, 4)
-puts entry.reading_chunk(1, 4)
-puts entry.reading_chunk(1, 4)
-puts entry.reading_chunk(1, 4)
-puts entry.reading_chunk(1, 4)
-puts entry.reading_chunk(1, 4)
+diary = DiaryEntry.new("Mon", "Hello hello")
+puts diary.count_words.class
--- spec/diary_entry_spec.rb
+++ spec/diary_entry_spec.rb
@@ -1,57 +1,50 @@
 require "diary_entry"
 
 describe DiaryEntry do
-  context "title method" do
-    it "returns the title as a string" do
-      diary_entry = DiaryEntry.new("Wednesday", "abc")
-      result = diary_entry.title
-      expect(result).to eq "Wednesday"
-    end
+  it "returns the title as a string" do
+    diary_entry = DiaryEntry.new("Wednesday", "abc")
+    result = diary_entry.title
+    expect(result).to eq "Wednesday"
   end
-  context "contents method" do
-    it "returns the contents as a string" do
-      diary_entry = DiaryEntry.new("Wednesday", "abc")
-      result = diary_entry.contents
-      expect(result).to eq "abc"
-    end
+
+  it "returns the contents as a string" do
+    diary_entry = DiaryEntry.new("Wednesday", "abc")
+    result = diary_entry.contents
+    expect(result).to eq "abc"
   end
-  context "count_words method" do
-    it "returns the number of words in the contents as an integer" do
-      diary_entry = DiaryEntry.new("Wednesday", "abc")
-      result = diary_entry.count_words
-      expect(result).to eq 1
-    end
+
+  it "returns the number of words in the contents as an integer" do
+    diary_entry = DiaryEntry.new("Wednesday", "abc")
+    result = diary_entry.count_words
+    expect(result).to eq 1
   end
-  context "reading_time method" do
-    it "returns an integer representing an estimate of the reading time in minutes for the contents at the given wpm" do
-      diary_entry = DiaryEntry.new("Wednesday", "abc")
-      result = diary_entry.reading_time(2)
-      expect(result).to eq 0
-    end
+
+  it "returns an integer representing an estimate of the reading time in minutes for the contents at the given wpm" do
+    diary_entry = DiaryEntry.new("Wednesday", "abc")
+    result = diary_entry.reading_time(2)
+    expect(result).to eq 0
   end
-  context "reading_chunk method" do
-    it "Returns a string with a chunk of the contents that the user could read in the given number of minutes" do
-      diary_entry =
-        DiaryEntry.new(
-          "Wednesday",
-          "Today I paired with Delphine and it was really nice."
-        )
-      result = diary_entry.reading_chunk(1, 5)
-      expect(result).to eq "Today I paired with Delphine"
-    end
+
+  it "Returns a string with a chunk of the contents that the user could read in the given number of minutes" do
+    diary_entry =
+      DiaryEntry.new(
+        "Wednesday",
+        "Today I paired with Delphine and it was really nice."
+      )
+    result = diary_entry.reading_chunk(1, 5)
+    expect(result).to eq "Today I paired with Delphine"
   end
-  context "reading_chunk method" do
-    it "if called multiple times it reads a further chunk of text until it has all been read" do
-      entry =
-        DiaryEntry.new(
-          "Sunday",
-          "Today is really hot so I went and got some green juice. It was really good and I will probably get some next week as well."
-        )
-      entry.reading_chunk(1, 4)
-      entry.reading_chunk(1, 4)
-      entry.reading_chunk(1, 4)
-      result = entry.reading_chunk(1, 4)
-      expect(result).to eq "It was really good"
-    end
+
+  it "if called multiple times it reads a further chunk of text until it has all been read" do
+    entry =
+      DiaryEntry.new(
+        "Sunday",
+        "Today is really hot so I went and got some green juice. It was really good and I will probably get some next week as well."
+      )
+    entry.reading_chunk(1, 4)
+    entry.reading_chunk(1, 4)
+    entry.reading_chunk(1, 4)
+    result = entry.reading_chunk(1, 4)
+    expect(result).to eq "It was really good"
   end
 end
